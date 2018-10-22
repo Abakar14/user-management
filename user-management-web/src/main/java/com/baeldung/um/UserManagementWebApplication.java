861695472aa2b4039c607aedba477b3635ebc628
abakar14 <abakar61@web.de>
2018-10-22 22:13:55 +0200

    This is a new Project for learing Rest with spring

---
+++ user-management-web/src/main/java/com/baeldung/um/UserManagementWebApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung.um;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementWebApplication.class, args);
+	}
+}
