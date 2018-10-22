957010734fc3ad270cba5a7eafe4dddcfa99c2df
abakar14 <abakar61@web.de>
2018-10-22 23:58:03 +0200

    parent pom updated

--- user-management-web/src/main/java/com/baeldung/um/UmApp.java
+++ user-management-web/src/main/java/com/baeldung/um/UmApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserManagementWebApplication {
+public class UmApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserManagementWebApplication.class, args);
+		SpringApplication.run(UmApp.class, args);
 	}
 }
--- user-management-web/src/test/java/com/baeldung/um/UmAppTests.java
+++ user-management-web/src/test/java/com/baeldung/um/UmAppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UserManagementWebApplicationTests {
+public class UmAppTests {
 
 	@Test
 	public void contextLoads() {
