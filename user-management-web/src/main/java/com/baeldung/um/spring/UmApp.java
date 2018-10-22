a401a0753e33e6a61e39e0ea9aef533626dfc40e
abakar14 <abakar61@web.de>
2018-10-23 00:49:48 +0200

    Change

--- user-management-web/src/main/java/com/baeldung/um/spring/UmApp.java
+++ user-management-web/src/main/java/com/baeldung/um/spring/UmApp.java
@@ -2,8 +2,17 @@ package com.baeldung.um.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ //formatter:off
+    UmContextConfig.class,
+    UmPersistenceJpaConfig.class,
+    UmServiceConfig.class,
+    UmWebConfig.class
+    
+    
+})//formatter:on
 public class UmApp {
 
 	public static void main(String[] args) {
