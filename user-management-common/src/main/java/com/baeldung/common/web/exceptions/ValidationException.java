39b934131760c59026d2c90d2d55e94480fab7bf
abakar14 <abakar61@web.de>
2018-10-22 23:33:05 +0200

    Change in pom and neue packages

---
+++ user-management-common/src/main/java/com/baeldung/common/web/exceptions/ValidationException.java
@@ -0,0 +1,17 @@
+package com.baeldung.common.web.exceptions;
+
+/**
+ * Thrown when validation conflict error is found. Message used to describe the validation error.
+ */
+public class ValidationException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException(final String message) {
+        super(message);
+    }
+
+}
