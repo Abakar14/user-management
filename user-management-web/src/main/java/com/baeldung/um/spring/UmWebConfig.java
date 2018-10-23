a3fdaad790848f7107df465e1ffe6be3775b2778
abakar14 <abakar61@web.de>
2018-10-23 22:02:54 +0200

    Change with comparison from original

--- user-management-web/src/main/java/com/baeldung/um/spring/UmWebConfig.java
+++ user-management-web/src/main/java/com/baeldung/um/spring/UmWebConfig.java
@@ -1,19 +1,9 @@
 package com.baeldung.um.spring;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 
 
@@ -28,23 +18,23 @@ public class UmWebConfig extends WebMvcConfigurationSupport {
     
     // configuration it used to formatted response to xml or json
     
-    @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-//    	// TODO Auto-generated method stub
-//    	super.extendMessageConverters(converters);
-    	   final Optional<HttpMessageConverter<?>> jsonConverterFound = converters.stream().filter(c -> c instanceof MappingJackson2HttpMessageConverter).findFirst();
-           if (jsonConverterFound.isPresent()) {
-               final AbstractJackson2HttpMessageConverter converter = (AbstractJackson2HttpMessageConverter) jsonConverterFound.get();
-               converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-               converter.getObjectMapper().enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-           }
-           final Optional<HttpMessageConverter<?>> xmlConverterFound = converters.stream().filter(c -> c instanceof MappingJackson2XmlHttpMessageConverter).findFirst();
-           if (xmlConverterFound.isPresent()) {
-               final MappingJackson2XmlHttpMessageConverter converter = (MappingJackson2XmlHttpMessageConverter) xmlConverterFound.get();
-               converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-               converter.getObjectMapper().enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-           }
-    }
+//    @Override
+//    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+////    	// TODO Auto-generated method stub
+////    	super.extendMessageConverters(converters);
+//    	   final Optional<HttpMessageConverter<?>> jsonConverterFound = converters.stream().filter(c -> c instanceof MappingJackson2HttpMessageConverter).findFirst();
+//           if (jsonConverterFound.isPresent()) {
+//               final AbstractJackson2HttpMessageConverter converter = (AbstractJackson2HttpMessageConverter) jsonConverterFound.get();
+//               converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+//               converter.getObjectMapper().enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+//           }
+//           final Optional<HttpMessageConverter<?>> xmlConverterFound = converters.stream().filter(c -> c instanceof MappingJackson2XmlHttpMessageConverter).findFirst();
+//           if (xmlConverterFound.isPresent()) {
+//               final MappingJackson2XmlHttpMessageConverter converter = (MappingJackson2XmlHttpMessageConverter) xmlConverterFound.get();
+//               converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+//               converter.getObjectMapper().enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+//           }
+//    }
 
   
 }
